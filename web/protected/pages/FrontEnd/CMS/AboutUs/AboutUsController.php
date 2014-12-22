4f767dee71126fe7d01c73d612bfefa0116798cc
Frank <tlan16@sina.com>
2014-12-22 15:44:29 +1100

    about us

--- web/protected/pages/FrontEnd/CMS/AboutUs/AboutUsController.php
+++ web/protected/pages/FrontEnd/CMS/AboutUs/AboutUsController.php
@@ -15,6 +15,6 @@ class AboutUsController extends FrontEndPageAbstract
     protected function _getEndJs()
     {
         $js = parent::_getEndJs();
-        return $js;
+        return $js;SystemSettings
     }
 }
