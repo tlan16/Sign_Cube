d4bd4097efbb2842126f6c8215987c8d1dc83943
Frank <tlan16@sina.com>
2015-01-20 14:16:07 +1100

    language list/new

--- web/protected/classes/BackEndPageAbstract/BackEndPageAbstract.1.js
+++ web/protected/classes/BackEndPageAbstract/BackEndPageAbstract.1.js
@@ -3,10 +3,183 @@
  */
 var BackEndPageJs = new Class.create();
 BackEndPageJs.prototype = Object.extend(new FrontPageJs(), {
-	_htmlIDs: {}
-
-	,setHTMLIDs: function(htmlIds) {
-		this._htmlIDs = htmlIds;
+	resultDivId: '' //the html id of the result div
+	,searchDivId: '' //the html id of the search div
+	,totalNoOfItemsId: '' //the html if of the total no of items
+	,_pagination: {'pageNo': 1, 'pageSize': 30} //the pagination details
+	,_searchCriteria: {} //the searching criteria
+	,setHTMLIds: function(resultDivId, searchDivId, totalNoOfItemsId) {
+		this.resultDivId = resultDivId;
+		this.searchDivId = searchDivId;
+		this.totalNoOfItemsId = totalNoOfItemsId;
 		return this;
+	}
+	,getSearchCriteria: function() {
+		var tmp = {};
+		tmp.me = this;
+		if(tmp.me._searchCriteria === null)
+			tmp.me._searchCriteria = {};
+		tmp.nothingTosearch = true;
+		$(tmp.me.searchDivId).getElementsBySelector('[search_field]').each(function(item) {
+			tmp.me._searchCriteria[item.readAttribute('search_field')] = $F(item);
+			if(($F(item) instanceof Array && $F(item).size() > 0) || (typeof $F(item) === 'string' && !$F(item).blank()))
+				tmp.nothingTosearch = false;
+		});
+		if(tmp.nothingTosearch === true)
+			tmp.me._searchCriteria = null;
+		return this;
+	}
+	,getResults: function(reset, pageSize) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.reset = (reset || false);
+		tmp.resultDiv = $(tmp.me.resultDivId);
+		
+		if(tmp.reset === true)
+			tmp.me._pagination.pageNo = 1;
+		tmp.me._pagination.pageSize = (pageSize || tmp.me._pagination.pageSize);
+		tmp.me.postAjax(tmp.me.getCallbackId('getItems'), {'pagination': tmp.me._pagination, 'searchCriteria': tmp.me._searchCriteria}, {
+			'onLoading': function () {
+				jQuery('#' + tmp.me.searchDivId + ' #searchBtn').button('loading');
+				//reset div
+				if(tmp.reset === true) {
+					tmp.resultDiv.update( new Element('tr').update( new Element('td').update( tmp.me.getLoadingImg() ) ) );
+				}
+			}
+			,'onSuccess': function(sender, param) {
+				try{
+					tmp.result = tmp.me.getResp(param, false, true);
+					if(!tmp.result)
+						return;
+					$(tmp.me.totalNoOfItemsId).update(tmp.result.pageStats.totalRows);
+					
+					//reset div
+					if(tmp.reset === true) {
+						tmp.resultDiv.update(tmp.me._getResultRow(tmp.me._getTitleRowData(), true).wrap(new Element('thead')));
+					}
+					//remove next page button
+					tmp.resultDiv.getElementsBySelector('.paginWrapper').each(function(item){
+						item.remove();
+					});
+					
+					//show all items
+					tmp.tbody = $(tmp.resultDiv).down('tbody');
+					if(!tmp.tbody)
+						$(tmp.resultDiv).insert({'bottom': tmp.tbody = new Element('tbody') });
+					tmp.result.items.each(function(item) {
+						tmp.tbody.insert({'bottom': tmp.me._getResultRow(item).addClassName('item_row').writeAttribute('item_id', item.id) });
+					});
+					//show the next page button
+					if(tmp.result.pageStats.pageNumber < tmp.result.pageStats.totalPages)
+						tmp.resultDiv.insert({'bottom': tmp.me._getNextPageBtn().addClassName('paginWrapper') });
+				} catch (e) {
+					tmp.resultDiv.insert({'bottom': tmp.me.getAlertBox('Error', e).addClassName('alert-danger') });
+				}
+			}
+			,'onComplete': function() {
+				jQuery('#' + tmp.me.searchDivId + ' #searchBtn').button('reset');
+			}
+		});
+	}
+	,_saveItem: function(btn, savePanel, attrName) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.data = tmp.me._collectFormData(savePanel, attrName);
+		if(tmp.data === null)
+			return;
+		
+		tmp.me.postAjax(tmp.me.getCallbackId('saveItem'), {'item': tmp.data}, {
+			'onLoading': function () {
+				if(tmp.data.id) {
+					savePanel.addClassName('item_row').writeAttribute('item_id', tmp.data.id); 
+				}
+				savePanel.hide();
+			}
+			,'onSuccess': function(sender, param) {
+				try{
+					tmp.result = tmp.me.getResp(param, false, true);
+					if(!tmp.result || !tmp.result.item)
+						return;
+					tmp.row = $(tmp.me.resultDivId).down('tbody').down('.item_row[item_id=' + tmp.result.item.id + ']');
+					tmp.newRow = tmp.me._getResultRow(tmp.result.item).addClassName('item_row').writeAttribute('item_id', tmp.result.item.id);
+					if(!tmp.row)
+					{
+						$(tmp.me.resultDivId).down('tbody').insert({'top': tmp.newRow });
+						savePanel.remove();
+						$(tmp.me.totalNoOfItemsId).update($(tmp.me.totalNoOfItemsId).innerHTML * 1 + 1);
+					}
+					else
+					{
+						tmp.row.replace(tmp.newRow);
+					}
+				
+				} catch (e) {
+					tmp.me.showModalBox('<span class="text-danger">ERROR:</span>', e, true);
+					savePanel.show();
+				}
+			}
+		});
+		return tmp.me;
+	}
+	/**
+	 * Collecting data from attrName
+	 */
+	,_collectFormData: function(container, attrName, groupIndexName) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.data = {};
+		tmp.hasError = false;
+		$(container).getElementsBySelector('[' + attrName + ']').each(function(item) {
+			tmp.groupIndexName = groupIndexName ? item.readAttribute(groupIndexName) : null;
+			tmp.fieldName = item.readAttribute(attrName);
+			if(item.hasAttribute('required') && $F(item).blank()) {
+				tmp.me._markFormGroupError(item, 'This is requried');
+				tmp.hasError = true;
+			}
+			
+			tmp.itemValue = item.readAttribute('type') !== 'checkbox' ? $F(item) : $(item).checked;
+			if(item.hasAttribute('validate_currency') || item.hasAttribute('validate_number')) {
+				if (tmp.me.getValueFromCurrency(tmp.itemValue).match(/^\d+(\.\d{1,2})?$/) === null) {
+					tmp.me._markFormGroupError(item, (item.hasAttribute('validate_currency') ? item.readAttribute('validate_currency') : item.hasAttribute('validate_number')));
+					tmp.hasError = true;
+				}
+				tmp.value = tmp.me.getValueFromCurrency(tmp.itemValue);
+			}
+			
+			//getting the data
+			if(tmp.groupIndexName !== null && tmp.groupIndexName !== undefined) {
+				if(!tmp.data[tmp.groupIndexName])
+					tmp.data[tmp.groupIndexName] = {};
+				tmp.data[tmp.groupIndexName][tmp.fieldName] = tmp.itemValue;
+			} else {
+				tmp.data[tmp.fieldName] = tmp.itemValue;
+			}
+		});
+		return (tmp.hasError === true ? null : tmp.data);
+	}
+	,_deactivateItem: function(btn) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.row = $(btn).up('[item_id]');
+		tmp.item = tmp.row.retrieve('data');
+		tmp.me.postAjax(tmp.me.getCallbackId('deactivateItems'), {'item_id': tmp.item.id}, {
+			'onLoading': function() {
+				if(tmp.row) {
+					tmp.row.toggleClassName('danger');
+					tmp.row.hide(); 
+				}
+			}
+			,'onSuccess': function(sender, param){
+				try {
+					tmp.row.toggleClassName('danger');
+					tmp.result = tmp.me.getResp(param, false, true);
+					if(!tmp.result.item)
+						throw 'errror';
+					tmp.row.replace(tmp.me._getResultRow(tmp.result.item, false));
+				} catch(e) {
+					tmp.me.showModalBox('<span class="text-danger">ERROR</span>', e, true);
+				}
+			}
+		});
 	}
 });
--- web/protected/pages/BackEnd/Language/List/Controller.1.js
+++ web/protected/pages/BackEnd/Language/List/Controller.1.js
@@ -3,34 +3,8 @@
  */
 var PageJs = new Class.create();
 PageJs.prototype = Object.extend(new BackEndPageJs(), {
-	resultDivId: '' //the html id of the result div
-	,searchDivId: '' //the html id of the search div
-	,totalNoOfItemsId: '' //the html if of the total no of items
-	,_pagination: {'pageNo': 1, 'pageSize': 30} //the pagination details
-	,_searchCriteria: {} //the searching criteria
-	,_getTitleRowData: function() {
-		return {'name': "Name", 'code': 'Code'};
-	}
-	,setHTMLIds: function(resultDivId, searchDivId, totalNoOfItemsId) {
-		this.resultDivId = resultDivId;
-		this.searchDivId = searchDivId;
-		this.totalNoOfItemsId = totalNoOfItemsId;
-		return this;
-	}
-	,getSearchCriteria: function() {
-		var tmp = {};
-		tmp.me = this;
-		if(tmp.me._searchCriteria === null)
-			tmp.me._searchCriteria = {};
-		tmp.nothingTosearch = true;
-		$(tmp.me.searchDivId).getElementsBySelector('[search_field]').each(function(item) {
-			tmp.me._searchCriteria[item.readAttribute('search_field')] = $F(item);
-			if(($F(item) instanceof Array && $F(item).size() > 0) || (typeof $F(item) === 'string' && !$F(item).blank()))
-				tmp.nothingTosearch = false;
-		});
-		if(tmp.nothingTosearch === true)
-			tmp.me._searchCriteria = null;
-		return this;
+	_getTitleRowData: function() {
+		return {'name': "Name", 'code': 'Code', 'active': 'Active?'};
 	}
 	,_bindSearchKey: function() {
 		var tmp = {}
@@ -50,10 +24,13 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 		tmp.newDiv = new Element('tr', {'class': 'save-item-panel info'}).store('data', row)
 			.insert({'bottom': new Element('input', {'type': 'hidden', 'save-item-panel': 'id', 'value': row.id ? row.id : ''}) })
 			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'required': true, 'class': 'form-control', 'placeholder': 'The name of the Lanugage', 'save-item-panel': 'name', 'value': row.name ? row.name : ''}) })
+				.insert({'bottom': new Element('input', {'required': true, 'class': 'form-control', 'placeholder': 'The Name of the Language', 'save-item-panel': 'name', 'value': row.name ? row.name : ''}) })
 			})
 			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'class': 'form-control', 'placeholder': 'The code of the Lanugage', 'save-item-panel': 'code', 'value': row.code ? row.code : ''}) })
+				.insert({'bottom': new Element('input', {'class': 'form-control', 'placeholder': 'The Code of the Language', 'save-item-panel': 'code', 'value': row.code ? row.code : ''}) })
+			})
+			.insert({'bottom': new Element('td', {'class': 'form-group'})
+				.insert({'bottom': new Element('input', {'type': 'checkbox', 'class': 'form-control', 'save-item-panel': 'active', 'checked': row.active}) })
 			})
 			.insert({'bottom': new Element('td', {'class': 'text-right'})
 				.insert({'bottom':  new Element('span', {'class': 'btn-group btn-group-sm'})
@@ -74,7 +51,7 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 						})
 					})
 				})
-			})
+			});
 		return tmp.newDiv;
 	}
 	,_getResultRow: function(row, isTitle) {
@@ -82,10 +59,13 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 		tmp.me = this;
 		tmp.tag = (tmp.isTitle === true ? 'th' : 'td');
 		tmp.isTitle = (isTitle || false);
-		tmp.row = new Element('tr', {'class': (tmp.isTitle === true ? 'success' : 'btn-hide-row')}).store('data', row)
-			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-1', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.name) })
-			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-1', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.code) })
-			.insert({'bottom': new Element(tmp.tag, {'class': 'text-right btns col-xs-2 hidden'}).update(
+		tmp.row = new Element('tr', {'style': tmp.isTitle ? 'font-size:110%; font-weight:bold;' : '', 'class': (tmp.isTitle === true ? '' : (row.active ? 'btn-hide-row' : 'danger'))}).store('data', row)
+			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-5', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.name) })
+			.insert({'bottom': new Element(tmp.tag, {'class': 'code col-xs-4', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.code) })
+			.insert({'bottom': new Element(tmp.tag, {'class': 'active col-xs-1'})
+				.insert({'bottom': (tmp.isTitle === true ? row.active : new Element('input', {'type': 'checkbox', 'disabled': true, 'checked': row.active}) ) })
+			})
+			.insert({'bottom': new Element(tmp.tag, {'class': 'text-right btns col-xs-2'}).update(
 				tmp.isTitle === true ?  
 				(new Element('span', {'class': 'btn btn-primary btn-xs', 'title': 'New'})
 					.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-plus'}) })
@@ -104,8 +84,8 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 						});
 					})
 				)
-				: (new Element('span', {'class': 'btn-group btn-group-xs'})
-					.insert({'bottom': new Element('span', {'class': 'btn btn-default', 'title': 'Edit'})
+				: (new Element('span', {'class': row.active ? 'btn-group btn-group-xs' : ''})
+					.insert({'bottom': new Element('span', {'class': 'btn btn-xs btn-default', 'title': 'Edit'})
 						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-pencil'}) })
 						.observe('click', function(){
 							$(this).up('.item_row').replace(tmp.editEl = tmp.me._getEditPanel(row));
@@ -121,68 +101,16 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 							});
 						})
 					})
-					.insert({'bottom': new Element('span', {'class': 'btn btn-danger', 'title': 'Delete'})
+					.insert({'bottom': new Element('span', {'class': row.active ? 'btn btn-danger' : 'hidden', 'title': 'Delete'})
 						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-trash'}) })
 						.observe('click', function(){
 							if(!confirm('Are you sure you want to delete this item?'))
 								return false;
-							tmp.me._deleteItem(row);
+							tmp.me._deactivateItem($(this));
 						})
 					}) ) 
 			) })
 		;
 		return tmp.row;
-	}
-	,getResults: function(reset, pageSize) {
-		var tmp = {};
-		tmp.me = this;
-		tmp.reset = (reset || false);
-		tmp.resultDiv = $(tmp.me.resultDivId);
-		
-		if(tmp.reset === true)
-			tmp.me._pagination.pageNo = 1;
-		tmp.me._pagination.pageSize = (pageSize || tmp.me._pagination.pageSize);
-		tmp.me.postAjax(tmp.me.getCallbackId('getItems'), {'pagination': tmp.me._pagination, 'searchCriteria': tmp.me._searchCriteria}, {
-			'onLoading': function () {
-				jQuery('#' + tmp.me.searchDivId + ' #searchBtn').button('loading');
-				//reset div
-				if(tmp.reset === true) {
-					tmp.resultDiv.update( new Element('tr').update( new Element('td').update( tmp.me.getLoadingImg() ) ) );
-				}
-			}
-			,'onSuccess': function(sender, param) {
-				try{
-					tmp.result = tmp.me.getResp(param, false, true);
-					if(!tmp.result)
-						return;
-					$(tmp.me.totalNoOfItemsId).update(tmp.result.pageStats.totalRows);
-					
-					//reset div
-					if(tmp.reset === true) {
-						tmp.resultDiv.update(tmp.me._getResultRow(tmp.me._getTitleRowData(), true).wrap(new Element('thead')));
-					}
-					//remove next page button
-					tmp.resultDiv.getElementsBySelector('.paginWrapper').each(function(item){
-						item.remove();
-					});
-					
-					//show all items
-					tmp.tbody = $(tmp.resultDiv).down('tbody');
-					if(!tmp.tbody)
-						$(tmp.resultDiv).insert({'bottom': tmp.tbody = new Element('tbody') });
-					tmp.result.items.each(function(item) {
-						tmp.tbody.insert({'bottom': tmp.me._getResultRow(item).addClassName('item_row').writeAttribute('item_id', item.id) });
-					});
-					//show the next page button
-					if(tmp.result.pageStats.pageNumber < tmp.result.pageStats.totalPages)
-						tmp.resultDiv.insert({'bottom': tmp.me._getNextPageBtn().addClassName('paginWrapper') });
-				} catch (e) {
-					tmp.resultDiv.insert({'bottom': tmp.me.getAlertBox('Error', e).addClassName('alert-danger') });
-				}
-			}
-			,'onComplete': function() {
-				jQuery('#' + tmp.me.searchDivId + ' #searchBtn').button('reset');
-			}
-		});
 	}
 });
