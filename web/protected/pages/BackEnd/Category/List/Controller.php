06bc71fea705c227a1da8cf2ac4baf666379a89c
Frank <tlan16@sina.com>
2015-01-20 14:21:43 +1100

    new template for listing pages

--- web/protected/pages/BackEnd/Category/List/Controller.php
+++ web/protected/pages/BackEnd/Category/List/Controller.php
@@ -15,6 +15,8 @@ class Controller extends BackEndPageAbstract
 		$js .= "pageJs";
 		$js .= "._bindSearchKey()";
 		$js .= ".setCallbackId('getItems', '" . $this->getItemsBtn->getUniqueID() . "')";
+		$js .= ".setCallbackId('deactivateItems', '" . $this->deactivateItemsBtn->getUniqueID() . "')";
+		$js .= ".setCallbackId('saveItem', '" . $this->saveItemBtn->getUniqueID() . "')";
 		$js .= ".setHTMLIds('item-list', 'searchPanel', 'total-found-count')";
 		$js .= ".getResults(true, " . DaoQuery::DEFAUTL_PAGE_SIZE . ");";
 		return $js;
@@ -78,5 +80,74 @@ class Controller extends BackEndPageAbstract
 	{
 		return trim($this->_focusEntity);
 	}
+	 /**
+     * save the items
+     *
+     * @param unknown $sender
+     * @param unknown $param
+     * @throws Exception
+     *
+     */
+    public function deactivateItems($sender, $param)
+    {
+    	$results = $errors = array();
+    	try
+    	{
+    		$class = trim($this->_focusEntity);
+    		$id = isset($param->CallbackParameter->item_id) ? $param->CallbackParameter->item_id : array();
+    			
+    		$item = $class::get($id);
+    			
+    		$item->setActive(false)
+    			->save();
+    		$results['item'] = $item->getJson();
+    	}
+    	catch(Exception $ex)
+    	{
+    		$errors[] = $ex->getMessage() . $ex->getTraceAsString();
+    	}
+    	$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
+    }
+    /**
+     * save the items
+     *
+     * @param unknown $sender
+     * @param unknown $param
+     * @throws Exception
+     *
+     */
+    public function saveItem($sender, $param)
+    {
+    	$results = $errors = array();
+    	try
+    	{
+    		var_dump($param->CallbackParameter->item);
+    		$class = trim($this->_focusEntity);
+    		if(!isset($param->CallbackParameter->item))
+    			throw new Exception("System Error: no item information passed in!");
+    		$item = (isset($param->CallbackParameter->item->id) && ($item = $class::get($param->CallbackParameter->item->id)) instanceof $class) ? $item : null;
+    		$name = trim($param->CallbackParameter->item->name);
+    		$code = trim($param->CallbackParameter->item->code);
+    		$active = (!isset($param->CallbackParameter->item->active) || $param->CallbackParameter->item->active !== true ? false : true);
+    			
+    		if($item instanceof $class)
+    		{
+    			$item->setName($name)
+    			->setCode($code)
+    			->setActive($active)
+    			->save();
+    		}
+    		else
+    		{
+    			$item = $class::create($name, $code);
+    		}
+    		$results['item'] = $item->getJson();
+    	}
+    	catch(Exception $ex)
+    	{
+    		$errors[] = $ex->getMessage();
+    	}
+    	$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
+    }
 }
 ?>
--- web/protected/pages/BackEnd/DefType/List/Controller.1.js
+++ web/protected/pages/BackEnd/DefType/List/Controller.1.js
@@ -3,34 +3,8 @@
  */
 var PageJs = new Class.create();
 PageJs.prototype = Object.extend(new BackEndPageJs(), {
-	resultDivId: '' //the html id of the result div
-	,searchDivId: '' //the html id of the search div
-	,totalNoOfItemsId: '' //the html if of the total no of items
-	,_pagination: {'pageNo': 1, 'pageSize': 30} //the pagination details
-	,_searchCriteria: {} //the searching criteria
-	,_getTitleRowData: function() {
-		return {'name': "Name", 'code': 'Code'};
-	}
-	,setHTMLIds: function(resultDivId, searchDivId, totalNoOfItemsId) {
-		this.resultDivId = resultDivId;
-		this.searchDivId = searchDivId;
-		this.totalNoOfItemsId = totalNoOfItemsId;
-		return this;
-	}
-	,getSearchCriteria: function() {
-		var tmp = {};
-		tmp.me = this;
-		if(tmp.me._searchCriteria === null)
-			tmp.me._searchCriteria = {};
-		tmp.nothingTosearch = true;
-		$(tmp.me.searchDivId).getElementsBySelector('[search_field]').each(function(item) {
-			tmp.me._searchCriteria[item.readAttribute('search_field')] = $F(item);
-			if(($F(item) instanceof Array && $F(item).size() > 0) || (typeof $F(item) === 'string' && !$F(item).blank()))
-				tmp.nothingTosearch = false;
-		});
-		if(tmp.nothingTosearch === true)
-			tmp.me._searchCriteria = null;
-		return this;
+	_getTitleRowData: function() {
+		return {'name': "Name", 'code': 'Code', 'active': 'Active?'};
 	}
 	,_bindSearchKey: function() {
 		var tmp = {}
@@ -50,10 +24,13 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 		tmp.newDiv = new Element('tr', {'class': 'save-item-panel info'}).store('data', row)
 			.insert({'bottom': new Element('input', {'type': 'hidden', 'save-item-panel': 'id', 'value': row.id ? row.id : ''}) })
 			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'required': true, 'class': 'form-control', 'placeholder': 'The name of the Lanugage', 'save-item-panel': 'name', 'value': row.name ? row.name : ''}) })
+				.insert({'bottom': new Element('input', {'required': true, 'class': 'form-control', 'placeholder': 'The Name of the Language', 'save-item-panel': 'name', 'value': row.name ? row.name : ''}) })
 			})
 			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'class': 'form-control', 'placeholder': 'The code of the Lanugage', 'save-item-panel': 'code', 'value': row.code ? row.code : ''}) })
+				.insert({'bottom': new Element('input', {'class': 'form-control', 'placeholder': 'The Code of the Language', 'save-item-panel': 'code', 'value': row.code ? row.code : ''}) })
+			})
+			.insert({'bottom': new Element('td', {'class': 'form-group'})
+				.insert({'bottom': new Element('input', {'type': 'checkbox', 'class': 'form-control', 'save-item-panel': 'active', 'checked': row.active}) })
 			})
 			.insert({'bottom': new Element('td', {'class': 'text-right'})
 				.insert({'bottom':  new Element('span', {'class': 'btn-group btn-group-sm'})
@@ -74,43 +51,7 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 						})
 					})
 				})
-			})
-		return tmp.newDiv;
-	}
-	,_getEditPanel: function(row) {
-		var tmp = {};
-		tmp.me = this;
-		tmp.newDiv = new Element('tr', {'class': 'save-item-panel info'}).store('data', row)
-			.insert({'bottom': new Element('input', {'type': 'hidden', 'save-item-panel': 'id', 'value': row.id ? row.id : ''}) })
-			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'required': true, 'class': 'form-control', 'placeholder': 'The Name of the Language', 'save-item-panel': 'name', 'value': row.name ? row.name : ''}) })
-			})
-			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'class': 'form-control', 'placeholder': 'The Code of the Language', 'save-item-panel': 'code', 'value': row.code ? row.code : ''}) })
-			})
-			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'type': 'checkbox', 'class': 'form-control', 'save-item-panel': 'allowMultiple', 'checked': row.allowMultiple}) })
-			})
-			.insert({'bottom': new Element('td', {'class': 'text-right'})
-				.insert({'bottom':  new Element('span', {'class': 'btn-group btn-group-sm'})
-					.insert({'bottom': new Element('span', {'class': 'btn btn-success', 'title': 'Save'})
-						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-ok'}) })
-						.observe('click', function(){
-							tmp.btn = this;
-							tmp.me._saveItem(tmp.btn, $(tmp.btn).up('.save-item-panel'), 'save-item-panel');
-						})
-					})
-					.insert({'bottom': new Element('span', {'class': 'btn btn-danger', 'title': 'Delete'})
-						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-remove'}) })
-						.observe('click', function(){
-							if(row.id)
-								$(this).up('.save-item-panel').replace(tmp.me._getResultRow(row).addClassName('item_row').writeAttribute('item_id', row.id));
-							else
-								$(this).up('.save-item-panel').remove();
-						})
-					})
-				})
-			})
+			});
 		return tmp.newDiv;
 	}
 	,_getResultRow: function(row, isTitle) {
@@ -118,9 +59,12 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 		tmp.me = this;
 		tmp.tag = (tmp.isTitle === true ? 'th' : 'td');
 		tmp.isTitle = (isTitle || false);
-		tmp.row = new Element('tr', {'class': (tmp.isTitle === true ? 'success' : 'btn-hide-row')}).store('data', row)
-			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-1', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.name) })
-			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-1', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.code) })
+		tmp.row = new Element('tr', {'style': tmp.isTitle ? 'font-size:110%; font-weight:bold;' : '', 'class': (tmp.isTitle === true ? '' : (row.active ? 'btn-hide-row' : 'danger'))}).store('data', row)
+			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-5', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.name) })
+			.insert({'bottom': new Element(tmp.tag, {'class': 'code col-xs-4', 'style': tmp.isTitle ? 'font-weight:bold;' : ''}).update(row.code) })
+			.insert({'bottom': new Element(tmp.tag, {'class': 'active col-xs-1'})
+				.insert({'bottom': (tmp.isTitle === true ? row.active : new Element('input', {'type': 'checkbox', 'disabled': true, 'checked': row.active}) ) })
+			})
 			.insert({'bottom': new Element(tmp.tag, {'class': 'text-right btns col-xs-2'}).update(
 				tmp.isTitle === true ?  
 				(new Element('span', {'class': 'btn btn-primary btn-xs', 'title': 'New'})
@@ -140,8 +84,8 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 						});
 					})
 				)
-				: (new Element('span', {'class': 'btn-group btn-group-xs'})
-					.insert({'bottom': new Element('span', {'class': 'btn btn-default', 'title': 'Edit'})
+				: (new Element('span', {'class': row.active ? 'btn-group btn-group-xs' : ''})
+					.insert({'bottom': new Element('span', {'class': 'btn btn-xs btn-default', 'title': 'Edit'})
 						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-pencil'}) })
 						.observe('click', function(){
 							$(this).up('.item_row').replace(tmp.editEl = tmp.me._getEditPanel(row));
@@ -157,68 +101,16 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 							});
 						})
 					})
-					.insert({'bottom': new Element('span', {'class': 'btn btn-danger', 'title': 'Delete'})
+					.insert({'bottom': new Element('span', {'class': row.active ? 'btn btn-danger' : 'hidden', 'title': 'Delete'})
 						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-trash'}) })
 						.observe('click', function(){
 							if(!confirm('Are you sure you want to delete this item?'))
 								return false;
-							tmp.me._deleteItem(row);
+							tmp.me._deactivateItem($(this));
 						})
 					}) ) 
 			) })
 		;
 		return tmp.row;
-	}
-	,getResults: function(reset, pageSize) {
-		var tmp = {};
-		tmp.me = this;
-		tmp.reset = (reset || false);
-		tmp.resultDiv = $(tmp.me.resultDivId);
-		
-		if(tmp.reset === true)
-			tmp.me._pagination.pageNo = 1;
-		tmp.me._pagination.pageSize = (pageSize || tmp.me._pagination.pageSize);
-		tmp.me.postAjax(tmp.me.getCallbackId('getItems'), {'pagination': tmp.me._pagination, 'searchCriteria': tmp.me._searchCriteria}, {
-			'onLoading': function () {
-				jQuery('#' + tmp.me.searchDivId + ' #searchBtn').button('loading');
-				//reset div
-				if(tmp.reset === true) {
-					tmp.resultDiv.update( new Element('tr').update( new Element('td').update( tmp.me.getLoadingImg() ) ) );
-				}
-			}
-			,'onSuccess': function(sender, param) {
-				try{
-					tmp.result = tmp.me.getResp(param, false, true);
-					if(!tmp.result)
-						return;
-					$(tmp.me.totalNoOfItemsId).update(tmp.result.pageStats.totalRows);
-					
-					//reset div
-					if(tmp.reset === true) {
-						tmp.resultDiv.update(tmp.me._getResultRow(tmp.me._getTitleRowData(), true).wrap(new Element('thead')));
-					}
-					//remove next page button
-					tmp.resultDiv.getElementsBySelector('.paginWrapper').each(function(item){
-						item.remove();
-					});
-					
-					//show all items
-					tmp.tbody = $(tmp.resultDiv).down('tbody');
-					if(!tmp.tbody)
-						$(tmp.resultDiv).insert({'bottom': tmp.tbody = new Element('tbody') });
-					tmp.result.items.each(function(item) {
-						tmp.tbody.insert({'bottom': tmp.me._getResultRow(item).addClassName('item_row').writeAttribute('item_id', item.id) });
-					});
-					//show the next page button
-					if(tmp.result.pageStats.pageNumber < tmp.result.pageStats.totalPages)
-						tmp.resultDiv.insert({'bottom': tmp.me._getNextPageBtn().addClassName('paginWrapper') });
-				} catch (e) {
-					tmp.resultDiv.insert({'bottom': tmp.me.getAlertBox('Error', e).addClassName('alert-danger') });
-				}
-			}
-			,'onComplete': function() {
-				jQuery('#' + tmp.me.searchDivId + ' #searchBtn').button('reset');
-			}
-		});
 	}
 });
