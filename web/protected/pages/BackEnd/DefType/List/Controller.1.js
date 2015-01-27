d736b67169bd1093788fbbf2f33a483c189e293e
Frank <tlan16@sina.com>
2015-01-27 19:11:43 +1100

    defType listing bug fixed

--- web/protected/pages/BackEnd/DefType/List/Controller.1.js
+++ web/protected/pages/BackEnd/DefType/List/Controller.1.js
@@ -21,6 +21,7 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 	,_getEditPanel: function(row) {
 		var tmp = {};
 		tmp.me = this;
+		console.debug(row);
 		tmp.newDiv = new Element('tr', {'class': 'save-item-panel info'}).store('data', row)
 			.insert({'bottom': new Element('input', {'type': 'hidden', 'save-item-panel': 'id', 'value': row.id ? row.id : ''}) })
 			.insert({'bottom': new Element('td', {'class': 'form-group'})
@@ -28,7 +29,7 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 			})
 			
 			.insert({'bottom': new Element('td', {'class': 'form-group'})
-				.insert({'bottom': new Element('input', {'type': 'checkbox', 'class': 'form-control', 'save-item-panel': 'active', 'checked': row.active}) })
+				.insert({'bottom': new Element('input', {'type': 'checkbox', 'class': 'form-control', 'save-item-panel': 'active', 'checked': row.id ? row.active : true}) })
 			})
 			.insert({'bottom': new Element('td', {'class': 'text-right'})
 				.insert({'bottom':  new Element('span', {'class': 'btn-group btn-group-sm'})
@@ -50,6 +51,8 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 					})
 				})
 			});
+		if(!row.id)
+			tmp.newDiv.down('input[type="checkbox"]').writeAttribute('disabled', true);
 		return tmp.newDiv;
 	}
 	,_getResultRow: function(row, isTitle) {
