7e80fbe03bb249073e595fb486e512e6caef903d
Frank <tlan16@sina.com>
2015-02-08 17:53:51 +1100

    Core: userAccount: exiprty

--- core/promotion/db_import.bat
+++ core/promotion/db_import.bat
@@ -1,7 +1,6 @@
 @echo off
 :: Remove Existing databases
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot -e "DROP DATABASE IF EXISTS signcube"
-Pause
 
 :: Create new databases
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot -e "CREATE DATABASE `signcube` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
@@ -9,14 +8,11 @@ Pause
 
 :: Import sql file
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\structure.sql
-Pause
+c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\userAccount.sql
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\data.sql
-Pause
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\auslan.sql
-Pause
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\language.sql
-Pause
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\category.sql
+::c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\frank_laptop.sql
+
 Pause
-#c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot signcube < .\frank_laptop.sql
-#Pause
---
+++ core/promotion/userAccount.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `useraccount`;
+CREATE TABLE `useraccount` (
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`username` varchar(100) NOT NULL DEFAULT '',
+	`password` varchar(40) NOT NULL DEFAULT '',
+	`expiry` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+	`personId` int(10) unsigned NOT NULL DEFAULT 0,
+	`confirmationId` int(10) unsigned NULL DEFAULT NULL,
+	`active` bool NOT NULL DEFAULT 1,
+	`created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+	`createdById` int(10) unsigned NOT NULL DEFAULT 0,
+	`updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	`updatedById` int(10) unsigned NOT NULL DEFAULT 0,
+	PRIMARY KEY (`id`)
+	,INDEX (`personId`)
+	,INDEX (`confirmationId`)
+	,INDEX (`createdById`)
+	,INDEX (`updatedById`)
+	,INDEX (`username`)
+	,INDEX (`password`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
