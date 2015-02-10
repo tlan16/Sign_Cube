f4c7c13c4a67f2ef84025da97957dbc25bacc759
Frank <tlan16@sina.com>
2015-02-10 13:24:08 +1100

    db import bash

--- core/promotion/db_import.bash
+++ core/promotion/db_import.bash
@@ -1,11 +1,8 @@
-mysql -u root -p signcube < structure.sql
-
-mysql -u root -p signcube < data.sql
-
-mysql -u root -p signcube < auslan.sql
-
-mysql -u root -p signcube < language.sql
-
-mysql -u root -p signcube < category.sql
-
-#mysql -u root -p signcube < frank_laptop.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/structure.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/userAccount.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/data.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/auslan.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/language.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/category.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/postUserAccount.sql
+mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/server.sql
