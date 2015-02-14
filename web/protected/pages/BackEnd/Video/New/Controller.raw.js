933dee60dadcffa8bbf2cb093311ce059e04447c
Frank <tlan16@sina.com>
2015-02-14 19:30:56 +1100

    new Video: multiple files

--- web/protected/pages/BackEnd/Video/New/Controller.raw.js
+++ web/protected/pages/BackEnd/Video/New/Controller.raw.js
@@ -26,7 +26,6 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 	                tmp.uploadErrors = [];
 	                // Video Type Limit
 	                tmp.acceptFileTypes = /^video\/(mp4)$/i; // only take mp4 so far since it's well browser supported
-	                console.debug(data);
 	                if(data.originalFiles && data.originalFiles.length != 1) {
 	                	tmp.me.showModalBox('Invalid File Quantity', 'Only one file per upload');
 	                }
@@ -43,10 +42,55 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 	        }
 			,done: function(event, data) {
 				console.debug(data);
-				$(tmp.me.uploaderDivId).hide();
-				$(tmp.me.uploaderContainerId).insert({'bottom': new Element('video', {'width': 320, 'height': 240, 'controls': true})
-					.insert({'bottom': new Element('source', {'src': data._response.result.asset.url, 'type': data._response.result.asset.mimeType}) })
+				jQuery('#progress .bar').css(
+			            'width', '0%'
+			        );
+				$(tmp.me.uploaderContainerId).down('.panel-body')
+					.insert({'bottom': new Element('div', {'class': 'row', 'video-id': data._response.result.video.id})
+						.insert({'bottom': new Element('div', {'class': 'col-md-4'})
+							.insert({'bottom': tmp.videoEl = new Element('video', {'class': 'video-js vjs-default-skin vjs-big-play-centered', 'width': 320, 'height': 240, 'controls': true, 'preload': 'auto', 'autoplay': true, 'loop': true})
+								.insert({'bottom': new Element('source', {'src': data._response.result.video.asset.url, 'type': data._response.result.video.asset.mimeType}) })
+							})
+						})
+						.insert({'bottom': new Element('div', {'class': 'col-md-8'})
+							.insert({'bottom': new Element('div', {'class': 'row'}).update('<b>Asset</b>') })
+							.insert({'bottom': tmp.assetKeyRow = new Element('div', {'class': 'row asset-key-row'}) })
+							.insert({'bottom': tmp.assetValueRow = new Element('div', {'class': 'row asset-value-row'}) })
+							.insert({'bottom': new Element('div', {'class': 'row clearfix'}).setStyle('border: 1px solid brown;') })
+							.insert({'bottom': new Element('div', {'class': 'row'}).update('<b>Video</b>') })
+							.insert({'bottom': tmp.videoKeyRow = new Element('div', {'class': 'row video-key-row'}) })
+							.insert({'bottom': tmp.videoValueRow = new Element('div', {'class': 'row video-value-row'}) })
+							.insert({'bottom': new Element('div', {'class': 'row btn-row btn-hide-row text-right'}) 
+								.insert({'bottom': new Element('div', {'class': 'btn btn-delete-video btn-danger btn-xs'})
+									.insert({'bottom': new Element('i', {'class': 'glyphicon glyphicon-trash'}) })
+									.observe('click',function(){
+										$(this).up('[video-id="' + data._response.result.video.id + '"]').hide();
+									})
+								})
+							})
+						})
+					});
+				tmp.me._signRandID(tmp.videoEl);
+				videojs($(tmp.videoEl.id), {}, function() { });
+				$H(data._response.result.video.asset).each(function(item){
+					tmp.assetKeyRow.insert({'bottom': new Element('span', {'class': ((item.key == 'filename' || item.key == 'assetId')  ? 'col-md-3' : 'col-md-1'), 'title': item.key}).setStyle('white-space: nowrap; overflow: hidden; text-overflow: clip;').update(item.key)});
+					tmp.assetValueRow.insert({'bottom': new Element('span', {'class': ((item.key == 'filename' || item.key == 'assetId') ? 'col-md-3' : 'col-md-1'), 'title': item.value}).setStyle('white-space: nowrap; overflow: hidden; text-overflow: ellipsis;').update(item.value)
+						.observe('dblclick', function(){
+							tmp.me.showModalBox(item.key, tmp.txtArea = new Element('input', {'value': item.value}).setStyle('width: 100%;').observe('keyup',function(){tmp.txtArea.value = item.value;}));
+							tmp.txtArea.focus(); tmp.txtArea.select();
+						})
+					});
 				});
+				$H(data._response.result.video).each(function(item){
+					tmp.videoKeyRow.insert({'bottom': new Element('span', {'class': 'col-md-1', 'title': item.key}).setStyle('white-space: nowrap; overflow: hidden; text-overflow: clip;').update(item.key)});
+					tmp.videoValueRow.insert({'bottom': new Element('span', {'class': 'col-md-1', 'title': item.value}).setStyle('white-space: nowrap; overflow: hidden; text-overflow: ellipsis;').update(item.value)
+						.observe('dblclick', function(){
+							tmp.me.showModalBox(item.key, tmp.txtArea = new Element('input', {'value': item.value}).setStyle('width: 100%;').observe('keyup',function(){tmp.txtArea.value = item.value;}));
+							tmp.txtArea.focus(); tmp.txtArea.select();
+						})
+					});
+				});
+				tmp.videoValueRow.insert({'top': new Element('input', {'class': 'hidden', 'value': data._response.result.video.id, 'save-item': 'videoId'})});
 			}
 			,fail: function (e, data) {
 				tmp.message = '';
@@ -71,7 +115,7 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 		tmp.newDiv = new Element('div', {'class': 'uploader', 'id': tmp.me.uploaderDivId})
 			.insert({'bottom': new Element('input', {'id': 'fileupload', 'type': 'file', 'name': "file"}) })
 			.insert({'bottom': new Element('div', {'id': 'progress', 'style': 'width: 100%; border: 1px solid black; height: 10px;'})
-				.insert({'bottom': new Element('div', {'class': 'bar', 'style': 'width: 1%; height: 100%; background-color: blue;'}) })
+				.insert({'bottom': new Element('div', {'class': 'bar', 'style': 'width: 0%; height: 100%; background-color: blue;'}) })
 			})
 		;
 		return tmp.newDiv;
@@ -79,7 +123,7 @@ PageJs.prototype = Object.extend(new BackEndPageJs(), {
 	,init: function() {
 		var tmp = {}
 		tmp.me = this;
-		$(tmp.me.uploaderContainerId).insert({'bottom': new Element('div')})
+		$(tmp.me.uploaderContainerId).down('.panel-head').insert({'bottom': new Element('div')})
 			.insert({'bottom': new Element('h4').update('New Video') })
 			.insert({'bottom': tmp.me.getUploaderDiv()})
 		;
