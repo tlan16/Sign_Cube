111efbb38c76815c0799cabd2ecd71e071036a8b
Frank <tlan16@sina.com>
2015-03-06 14:35:15 +1100

    no db bash

--- core/promotion/db_import.bash
+++
@@ -1,8 +0,0 @@
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/structure.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/userAccount.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/data.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/auslan.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/language.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/category.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/postUserAccount.sql
-mysql -u root -p signcube < /var/www/Sign_Cube/core/promotion/server.sql
